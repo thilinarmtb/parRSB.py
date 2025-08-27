c571d94a5e1170c890fddbec67e04b7eecf1dd38
Thilina Ratnayaka <thilinarmtb@gmail.com>
2025-08-27 19:07:26 +0000

    fix typos and bugs in the wrapper

--- src/parrsb.pyx
+++ src/parrsb.pyx
@@ -12,19 +12,19 @@ import numpy as np
 def conn_mesh(cnp.ndarray[cnp.float64_t, ndim=2] coord,
               cnp.ndarray[cnp.float64_t, ndim=2] pinfo,
               double tol, MPI.Comm comm):
-    cdef int ndof = coord.shape[0]
-    cdef int ndim = coord.shape[1]
+    cdef unsigned ndof = coord.shape[0]
+    cdef unsigned ndim = coord.shape[1]
     cdef int npi = pinfo.shape[0]
 
     # sanity checks: only 3d meshes are supported as of now.
     assert ndim == 3
     assert pinfo.shape[1] == 2
 
-    cdef int nv = 8
-    cdef int nelem = ndof // nv
+    cdef unsigned nv = 8
+    cdef unsigned nelem = ndof // nv
 
-    cdef double *coord_ = <double *>malloc(nelem * ndim * cython.sizeof(cython.double))
-    for e in range(nelem):
+    cdef double *coord_ = <double *>malloc(ndof * ndim * cython.sizeof(cython.double))
+    for e in range(ndof):
         for d in range(ndim):
             coord_[e * ndim + d] = coord[e, d]
 
@@ -33,7 +33,7 @@ def conn_mesh(cnp.ndarray[cnp.float64_t, ndim=2] coord,
         for d in range(2):
             pinfo_[e * 2 + d] = pinfo[e, d]
 
-    cdef long long *vtx_ = <long long *>malloc(nelem * nv * cython.sizeof(cython.longlong))
+    cdef long long *vtx_ = <long long *>malloc(ndof * cython.sizeof(cython.longlong))
     cdef int err = parrsb_conn_mesh(vtx_, coord_, nelem, ndim, pinfo_, npi, tol, comm.ob_mpi)
 
     vtx = np.zeros((nelem, nv))
