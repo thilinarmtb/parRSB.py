3a5e0204ac99e855f26d57a8863443dad77e9034
Thilina Ratnayaka <thilinarmtb@gmail.com>
2026-03-25 20:18:02 +0000

    Remove existing test

--- src/parrsb.pyx
+++ src/parrsb.pyx
@@ -1,6 +1,5 @@
 cimport cython
 cimport mpi4py.MPI as MPI
-cimport numpy as cnp
 from libc.stdlib cimport free, malloc
 from parrsb cimport parrsb_conn_mesh, parrsb_read_mesh
 import numpy as np
@@ -40,43 +39,3 @@ cdef class Mesh:
     def __dealloc__(self):
         free(self.bcs)
         free(self.coord)
-
-
-def conn_mesh(cnp.ndarray[cnp.float64_t, ndim=2] coord,
-              cnp.ndarray[cnp.float64_t, ndim=2] pinfo,
-              double tol, MPI.Comm comm):
-    cdef unsigned ndof = coord.shape[0]
-    cdef unsigned ndim = coord.shape[1]
-    cdef int npi = pinfo.shape[0]
-
-    # sanity checks: only 3d meshes are supported as of now.
-    assert ndim == 3
-    assert pinfo.shape[1] == 2
-
-    cdef unsigned nv = 8
-    cdef unsigned nel = ndof // nv
-
-    cdef double *coord_ = <double *>malloc(ndof * ndim * cython.sizeof(cython.double))
-    for e in range(ndof):
-        for d in range(ndim):
-            coord_[e * ndim + d] = coord[e, d]
-
-    cdef long long *pi = <long long *>malloc(npi * 2 * cython.sizeof(cython.longlong))
-    for p in range(npi):
-        for d in range(2):
-            pi[e * 2 + d] = pinfo[e, d]
-
-    cdef long long *vtx_ = <long long *>malloc(ndof * cython.sizeof(cython.longlong))
-    cdef int err = parrsb_conn_mesh(vtx_, coord_, nel, ndim, pi, npi, tol, comm.ob_mpi)
-
-    vtx = np.zeros((nel, nv))
-    if err == 0:
-        for e in range(nel):
-            for v in range(nv):
-                vtx[e, v] = vtx_[e * nv + v]
-
-    free(pi)
-    free(coord_)
-    free(vtx_)
-
-    return vtx
