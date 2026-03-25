479d4a49995aa69bd632a8f00ebb0391f796b901
Thilina Ratnayaka <thilinarmtb@gmail.com>
2026-03-25 19:49:39 +0000

    Fix cython-lint failure

--- src/parrsb.pyx
+++ src/parrsb.pyx
@@ -54,28 +54,28 @@ def conn_mesh(cnp.ndarray[cnp.float64_t, ndim=2] coord,
     assert pinfo.shape[1] == 2
 
     cdef unsigned nv = 8
-    cdef unsigned nelem = ndof // nv
+    cdef unsigned nel = ndof // nv
 
     cdef double *coord_ = <double *>malloc(ndof * ndim * cython.sizeof(cython.double))
     for e in range(ndof):
         for d in range(ndim):
             coord_[e * ndim + d] = coord[e, d]
 
-    cdef long long *pinfo_ = <long long *>malloc(npi * 2 * cython.sizeof(cython.longlong))
+    cdef long long *pi = <long long *>malloc(npi * 2 * cython.sizeof(cython.longlong))
     for p in range(npi):
         for d in range(2):
-            pinfo_[e * 2 + d] = pinfo[e, d]
+            pi[e * 2 + d] = pinfo[e, d]
 
     cdef long long *vtx_ = <long long *>malloc(ndof * cython.sizeof(cython.longlong))
-    cdef int err = parrsb_conn_mesh(vtx_, coord_, nelem, ndim, pinfo_, npi, tol, comm.ob_mpi)
+    cdef int err = parrsb_conn_mesh(vtx_, coord_, nel, ndim, pi, npi, tol, comm.ob_mpi)
 
-    vtx = np.zeros((nelem, nv))
+    vtx = np.zeros((nel, nv))
     if err == 0:
-        for e in range(nelem):
+        for e in range(nel):
             for v in range(nv):
                 vtx[e, v] = vtx_[e * nv + v]
 
-    free(pinfo_)
+    free(pi)
     free(coord_)
     free(vtx_)
 
