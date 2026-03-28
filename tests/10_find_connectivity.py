b2511dce9e1c0dc347ac2dbb9419f9f893ff0d10
Thilina Ratnayaka <thilinarmtb@gmail.com>
2026-03-28 20:22:09 +0000

    Add a test for connectivity

---
+++ tests/10_find_connectivity.py
@@ -0,0 +1,12 @@
+import numpy as np
+from mpi4py import MPI
+from parrsb import Mesh
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        sys.exit(1)
+
+    comm = MPI.COMM_WORLD
+    m = Mesh(sys.argv[1], comm)
+    vtx = m.find_connectivity(0.2)
